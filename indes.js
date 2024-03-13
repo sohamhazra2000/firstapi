f5d2910a89222f194b534cc0f4387da18e610db6
sohamhazra2000 <sohamhazra15@gmail.com>
2024-03-13 22:13:23 +0530

    seven8

--- indes.js
+++ indes.js
@@ -23,7 +23,7 @@ app.get("/",(req,res)=>{
 app.post("/a",(req,res)=>{
     data=req.body
     let a;
-    connection.query("select * from employee where empid=?",data.empid,(err,response)=>{if(response){a=1}else{a=0}})
+    connection.query("select * from employee where empid=?",data.empid,(err,response)=>{if(!err){a=1}else{a=0}})
     if(a){(console.log("someting"))}
     connection.query("insert into employee set ?",data,(err,response)=>{
         if(err){res.send({msg:"error"})}
